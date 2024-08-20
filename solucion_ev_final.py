bf042d15a16dde92ceb0732c0f22709864209d68
manuel henriquez <mhenriquezperalta@gmail.com>
2024-08-19 23:34:54 -0400

    cambio de nombre archivo

--- solucion_ev_final.py
+++ solucion_ev_final.py
@@ -20,3 +20,33 @@ detalle_boletas = pd.read_csv("detalle_boletas.csv", encoding="latin-1", sep=","
 
 #3
 #a
+#print(detalle_boletas.loc[1:10])
+#detalle_boletas = detalle_boletas.loc[~(detalle_boletas["ID"].str.contains("4XXXXX")) & ~(detalle_boletas["Nombre"].str.contains("XXXX"))]
+print(detalle_boletas)
+
+#b
+#print(detalle_boletas["Fecha"].str.replace("_/","/"))
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace("!","")
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace("{","")
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace("-","")
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace(".","")
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace("-","")
+detalle_boletas["Fecha"] = detalle_boletas["Fecha"].str.replace(".","")
+print(detalle_boletas["Fecha"])
+
+
+#4
+
+#5
+
+#6
+#lista_productos = pd.read_csv("Lista productos.csv", encoding="UTF-8", sep=",")
+#print(lista_productos)
+
+#7
+
+
+#8
+
+
+#9
--- solución_ev_final.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-detalle_boletas = pd.read_csv("detalle_boletas.csv", encoding="UTF-8", 
